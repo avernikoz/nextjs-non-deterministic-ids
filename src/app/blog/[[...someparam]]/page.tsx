3b33ce65d7fe26bb713f3b11187f1c9d30c3e559
itsmile01face <it.smile01face@gmail.com>
2025-02-04 21:21:47 +0300

    two more pages

---
+++ src/app/blog/[[...someparam]]/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function BlogPage() {
+
+  return (
+    <div className="min-h-screen bg-app-dark">
+     page.tsx (blog/page.tsx)
+    </div>
+  );
+}
---
+++ src/app/single/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function SinglePage() {
+
+  return (
+    <div className="min-h-screen bg-app-dark">
+     page.tsx (single/page.tsx)
+    </div>
+  );
+}
